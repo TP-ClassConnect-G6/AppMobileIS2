e5877ee3c861c221e46b519939fdae3d6db93342
ArelyAgama <aagama@fi.uba.ar>
2025-04-15 00:19:29 -0300

    fixes

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,3 @@
-// import { useSession } from "@/contexts/session";
-// import { Redirect, Stack } from "expo-router";
-
-// export default function App() {
-//   const { session } = useSession();
-//   // if (!session) return <Redirect href="/(login)" />;
-//   // return <Redirect href="/(tabs)" />;
-//   return <Redirect href="/(login)" />;
-// }
-
 import { useSession } from "@/contexts/session";
 import { Redirect } from "expo-router";
 
